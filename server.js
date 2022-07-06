f2ad64569e230dd1cfe6f8d07e99df641e672b6f
Roger_Tech <rogerio.gss123@gmail.com>
2022-07-06 16:35:56 -0300

    working but not sending to the database

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
+const mySqlConnection = require('./dbService');
+const dbService = require('./dbService');
+
+var bodyParser = require('body-parser');
+var helmet = require('helmet');
+var rateLimit = require("express-rate-limit");
+var dotenv = require('dotenv');
+dotenv.config();
+
+
+
+
+
 app.set("view engine", "ejs")
 
 
@@ -15,8 +28,28 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
     res.render('register')
+    var name = req.query.name;
+    var email = req.query.email;
+    var password = req.query.password;
+    //console.log(name);
+    //console.log(email);
+    //console.log(password);
+
 })
 
+app.post('/register', (req, res, next) => {
+    var name = req.query.name;
+    var email = req.query.email;
+    var password = req.query.password;
+    console.log(name);
+    var sql = `INSERT INTO authentification (name, email, password) VALUES ('${name}', '${email}', '${password}')`;
+    mySqlConnection.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log('record inserted');
+        //req.flash('success', 'Data added successfully!');
+        res.redirect('/login');
+      });
+});
 
 
 
